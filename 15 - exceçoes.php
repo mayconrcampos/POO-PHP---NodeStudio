bb3b67d5589bdb7a72c93d97a3baed35af868436
mayconrcampos <maycon.campos@gmail.com>
2021-08-06 10:08:31 -0300

    Aula sobre tratamento de exceções

--- 15 - exceçoes.php
+++ 15 - exceçoes.php
@@ -1,2 +1,34 @@
 <?php
 
+// Vamos estudar sobre tratamento de exceções
+
+// Exceção é uma ocorrência anormal que afeta o funcionamento da aplicação
+// Exception é a classe base para todas as exceptions.
+// message, code, file, line
+
+class NewsLetter{
+
+    public function cadastrarEmail($email){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            throw new Exception("Este email é inválido!", 100);
+
+        }else{
+            echo "Email cadastrado com sucesso!";
+        }
+    }
+}
+
+$newsletter = new NewsLetter();
+
+try {
+    $newsletter->cadastrarEmail("maycon@@gmail.com");
+
+
+}catch(Exception $e) {
+    echo "ERRO Código               ". $e->getCode()."\n";
+    echo "ERRO Mensagem             ". $e->getMessage()."\n";
+    echo "ERRO Linha                ". $e->getLine()."\n";
+    echo "ERRO Arquivo              ". $e->getFile()."\n";
+}
+
+// Desta forma, temos informações suficientes para quando ocorrer o erro, a aplicação não pare de rodar e também possamos capturar as mensagens de erro para corrigir o código.
